b96caa5d49d381a3599a3dbdd222dcc80773c96c
Martin Zanoni <mza@relewise.com>
2022-12-09 11:55:59 +0100

    change path variable

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
     transpileDependencies: true,
-    base: '/relewise-demos-javascript/',
+    publicPath: '/relewise-demos-javascript/',
 });
